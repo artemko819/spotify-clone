58749a7c5e3b5631bc8d177dcc02df9eb1bded9c
artemko819 <artemuskov92@gmail.com>
2022-10-10 19:29:08 +0300

    initial project

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Error,Loader,SongCard } from '../components'
+import {genres} from '../assets/constans'
+const Discover = () => {
+  return (
+
+    <div>Discover</div>
+  )
+}
+
+export default Discover
